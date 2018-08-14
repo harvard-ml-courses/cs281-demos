09440f95dc79ec04605fee7b9ed26f550f5c6ec6
Alex Rush <sasha.rush@gmail.com>
2018-08-14 13:09:33 -0400

    cs281.py

--- cs281.py
+++ cs281.py
@@ -1,7 +1,17 @@
-import plotly.graph_objs as go
+def configure_plotly_browser_state():
+  import IPython
+  display(IPython.core.display.HTML('''
+        <script src="/static/components/requirejs/require.js"></script>
+        <script>
+          requirejs.config({
+            paths: {
+              base: '/static/base',
+              plotly: 'https://cdn.plot.ly/plotly-latest.min.js?noext',
+            },
+          });
+        </script>
+        '''))
 
-def scatter(**kwargs):
-    go.Scatter()
-    f = go.FigureWidget()
-    f.add_scatter(**kwargs)
-    return f
+import IPython
+IPython.get_ipython().events.register('pre_run_cell', configure_plotly_browser_state)
+init_notebook_mode()
